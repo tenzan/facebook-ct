f543e14ff9adbdd1e819f404d1f76022aa6e90b8
Askar <askar.karasaev@gmail.com>
2019-12-06 15:53:16 +0900

    Retrieving Posts Using a PostCollection Resource

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
+use App\Http\Resources\PostCollection;
 use App\Http\Resources\Post as PostResource;
 
 class PostController extends Controller
 {
+    public function index()
+    {
+        return new PostCollection(Post::all());
+    }
+
     public function store()
     {
         $data = request()->validate([
